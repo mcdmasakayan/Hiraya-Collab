aed24e984db85a5b56f70080aea2e43ccca07b3a
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-21 21:43:28 +0800

    update to machine.py

--- Ticketing System/model/machine.py
+++ Ticketing System/model/machine.py
@@ -1,8 +1,35 @@
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector as mysql
 
-db = SQLAlchemy()
+db = mysql.connect(host='localhost', port=3306, username='root', passw='root')
+crsr = db.cursor()
+crsr.execute("CREATE DATABASE IF NOT EXISTS tixsys")
 
-class InsertTable(db.Model):
-    __tablename__ = 'tixsys'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.Integer, primary_key=True, autoincrement=True)
+def initiate_database():
+    db = mysql.connect(host='localhost', port=3306, username='root', passw='root', database="tixsys")
+    crsr = db.cursor()
+
+    crsr.execute("""CREATE TABLE IF NOT EXISTS tixsys_accounts VALUES (email VARCHAR(255),
+                 username VARCHAR(255), password VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255),
+                 verified BOOLEAN, archived BOOLEAN)""")
+    
+    print("SYSTEM: Database connection sucessful.")
+
+def login_user():
+    class User:
+        email = ""
+        username = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        verified = False
+        archived = False
+
+    user = User()
+    
+    crsr.execute("""INSERT INTO tixsys_accounts (email, username, password, first_name,
+                 last_name, verified, archived) VALUES (%s, %s, %s, %s, %s, %s, %s)""",
+                 (user.email, user.username, user.password, user.first_name, user.last_name,
+                  user.verified, user.archived))
+    
+    print("SYSTEM: Database update sucessful.")
+    
