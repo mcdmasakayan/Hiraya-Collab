f3c4cdf41a3f216ed5837f3e674a41ba715315b3
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-21 23:13:46 +0800

    update service.py

--- Ticketing System/view/service.py
+++ Ticketing System/view/service.py
@@ -4,7 +4,13 @@ def index_logic():
     return render_template("index.html", page="Index")
 
 def login_logic():
-    return render_template("login_page.html", page="Login")
+    status = "Login Successful!"
+    print("SYSTEM: Account found in database.")
+
+    return render_template("login_page.html", page="Login", status=status)
 
 def register_logic():
-    return render_template("register_page.html", page="Register")
+    status = "Registration Successful!"
+    print("SYSTEM: Account inserted in database.")
+
+    return render_template("register_page.html", page="Register", status=status)
