8b1864276109bf999e92811221f028932b92ae03
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-22 13:27:39 +0800

    Update control.py

--- Ticketing System/controller/control.py
+++ Ticketing System/controller/control.py
@@ -10,7 +10,7 @@ def login():
     if request.method == "POST":
         username = request.form['username-entry']
         password = request.form['password-entry']
-        status, logged_in = login_user(username, password)
+        status = login_user(username, password)
 
     return login_logic(status)
 
