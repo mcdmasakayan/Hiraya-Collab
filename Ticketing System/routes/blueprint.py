94f782bd03e8ac5a126a21c8a8d25deb2f38e8d2
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-27 12:28:03 +0800

    change all files to update

--- Ticketing System/routes/blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controller.control import index, login, register
-
-blueprint = Blueprint('blueprint', __name__)
-
-blueprint.route('/', methods=['GET'])(index)
-blueprint.route('/login', methods=['POST', 'GET'])(login)
-blueprint.route('/register', methods=['POST', 'GET'])(register)
