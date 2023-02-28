f5d81304ecf27816bf3512e966121420ca76c46f
BS4yson <124108572+BS4yson@users.noreply.github.com>
2023-02-28 15:12:35 +0800

    Zxc

--- Ticketing System/model/machine.py
+++ Ticketing System/model/machine.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy_utils import database_exists, create_database
 from datetime import datetime
 
@@ -15,7 +15,7 @@ class User(db.Model):
     email = db.Column(db.VARCHAR(255), unique=True, nullable=False)
     username = db.Column(db.VARCHAR(255), unique=True, nullable=False)
     password = db.Column(db.VARCHAR(255), nullable=False)
-    first_name = db.Column(db.VARCHAR(2         55), nullable=False)
+    first_name = db.Column(db.VARCHAR(255), nullable=False)
     last_name = db.Column(db.VARCHAR(255), nullable=False)
     verified = db.Column(db.Boolean, nullable=False)
     archived = db.Column(db.Boolean, nullable=False)
@@ -24,10 +24,10 @@ class Project(db.Model):
     __tablename__ = 'projects'
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     #organization = db.Column(db.Integer, foreign_key=True)
-    #user_id = db.Column(db.Integer, foreign_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user._id'))
     name = db.Column(db.VARCHAR(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    priority_level = db.Column(db.Integer, nulllable=False)
+    priority_level = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     date_updated = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     date_start = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
@@ -46,7 +46,7 @@ class Task(db.Model):
     #user_id = db.Column(db.Integer, foreign_key=True)
     name = db.Column(db.VARCHAR(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    priority_level = db.Column(db.Integer, nulllable=False)
+    priority_level = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     date_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.now)
     date_start = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
@@ -65,7 +65,7 @@ class Subtask(db.Model):
     #user_id = db.Column(db.Integer, foreign_key=True)
     name = db.Column(db.VARCHAR(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    priority_level = db.Column(db.Integer, nulllable=False)
+    priority_level = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     date_updated = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.now)
     date_start = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
