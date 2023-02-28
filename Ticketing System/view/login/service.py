89ac9a17a2e821ab160138cc841c09468ccc97c1
Marc Chester Masakayan <86514651+mcdmasakayan@users.noreply.github.com>
2023-02-28 12:53:29 +0800

    a

--- Ticketing System/view/login/service.py
+++ Ticketing System/view/login/service.py
@@ -11,21 +11,25 @@ def login_logic():
         password = request.args.get('password')
         pw_hash = hash_string(password)
 
-        for x in users.query.all():
-            if x.username == username and x.password == pw_hash:
-                auth = 1
-                msg = "SYSTEM: Account found in database."
-                break
-            else:
-                auth = 0
-                msg = "SYSTEM: Account not found in database."
+        if (bool(users.query.all()) == False):
+            auth = 0
+            msg = "SYSTEM: Account not found in database."
+        else:
+            for x in users.query.all():
+                if x.username == username and x.password == pw_hash:
+                    auth = 1
+                    msg = "SYSTEM: Account found in database."
+                    break
+                else:
+                    auth = 0
+                    msg = "SYSTEM: Account not found in database."
 
     except (UnboundLocalError, AttributeError):
         auth = 0
         msg = "An error has occurred."
 
     return jsonify({"auth":auth,
-                        "message":msg})
+                    "message":msg})
 
 def register_logic():
     try:
@@ -37,8 +41,6 @@ def register_logic():
         pw_hash = hash_string(password)
         user = users(email=email, username=username, password=pw_hash, first_name=first_name,
                             last_name=last_name, verified=False, archived=False)
-        auth = 0
-        msg = ""
 
         if (bool(users.query.all()) == False):
             db.session.add(user)
